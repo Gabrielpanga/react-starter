078cca1db5e4ed7fc72cf4063ddf601d55ac94d1
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-11-07 17:18:09 +0100

    Add tests configuration CI

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
-  roots: ["<rootDir>/tests"],
+  roots: ['<rootDir>/tests'],
 
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
   },
 
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
   // extended assertions to Jest
-  setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
-  ],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 
   // Test spec file resolution pattern
   // Matches parent folder `tests` and filename
   // should contain `test` or `spec`.
-  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.tsx?$",
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
 
   // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  reporters: ['default', 'jest-junit'],
 };
